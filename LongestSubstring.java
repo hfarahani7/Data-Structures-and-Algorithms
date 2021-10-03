5ecb6cf519adfeaceb1147da7b928bdde403833c
hfarahani7 <64180041+hfarahani7@users.noreply.github.com>
2021-10-03 12:07:53 -0400

    Create LongestSubstring.java

---
+++ LongestSubstring.java
@@ -0,0 +1,35 @@
+import java.lang.StringBuilder;
+
+class LongestSubstring {
+    public static int lengthOfLongestSubstring(String s) {
+        int i = 0;
+        int j = 0;
+        int maxLength = 0;
+        StringBuilder sb = new StringBuilder();
+        
+        if(s.length() < 2){
+            return(s.length());
+        }
+        
+        sb.append(Character.toString(s.charAt(0)));
+        while(i < s.length() - 1){
+            i++;
+            if(sb.indexOf(Character.toString(s.charAt(i))) == -1){
+                sb.append(Character.toString(s.charAt(i)));
+            }else{
+                while(sb.indexOf(Character.toString(s.charAt(i))) != -1){
+                    sb.delete(0, 1);
+                    j++;
+                }
+                sb.append(Character.toString(s.charAt(i)));
+            }
+            if(i - j + 1 > maxLength){
+                maxLength = i - j + 1;//should this have +1?
+            }
+        }
+        return(maxLength);
+    }
+    public static void main(String[] args){
+        System.out.println(lengthOfLongestSubstring("abcabcbb"));
+    }
+}
