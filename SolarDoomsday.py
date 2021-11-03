bf0ac780f5b74e8ab4f86011813d5af05c534813
hfarahani7 <64180041+hfarahani7@users.noreply.github.com>
2021-11-02 21:08:23 -0400

    Update SolarDoomsday.py

--- SolarDoomsday.py
+++ SolarDoomsday.py
@@ -1,16 +1,8 @@
+    # For google Foobar challenge
+    # return list of the areas of the largest squares that can be made out of the provided area, largest first
 import math
 
-def solution1(area):
-    # return list of the areas of the largest squares
-    # that can be made out of the provided area, largest first
-    if area < 1 or area > 1000000:
-        raise ValueError("area should be between 1 and 1000000 inclusive")
-    if math.sqrt(area).is_integer():
-        return [area]
-    biggest = int(math.sqrt(area)) ** 2
-    return [biggest] + solution1(area - biggest)
-
-def solution2(area):
+def solution(area):
     ar = area
     list_squares = []
     
@@ -25,4 +17,4 @@ def solution2(area):
     return(list_squares)
 
 area = 11000
-print(solution2(area))
+print(solution(area))
